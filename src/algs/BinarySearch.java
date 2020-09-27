71589d0a560b5377ac4f991f146a9db56bffa676
peiyilin <peiyilin@xdf.cn>
2020-09-27 11:48:34 +0800

    feat: 算法 - 二分查找的四种变体实现

--- src/algs/BinarySearch.java
+++ src/algs/BinarySearch.java
@@ -60,4 +60,104 @@ public class BinarySearch {
             return binarySearchRecursive(sortedArray,low,mid-1,target);
         }
     }
+
+    /**
+     * 变体1：查找第一个值等于给定值的元素
+     * @param sortedArray
+     * @param target
+     * @return
+     */
+    public int binarySearchVariant1(int[] sortedArray,int target){
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (low <= high){
+            int mid = low + ((high - low) >> 1);
+            if (sortedArray[mid] > target){
+                high = mid - 1;
+            }else if (sortedArray[mid] < target){
+                low = mid + 1;
+            }else {
+                if (mid == 0 || sortedArray[mid-1] != sortedArray[mid]){
+                    return mid;
+                }else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 变体2：查找最后一个值等于给定值的元素
+     * @param sortedArray
+     * @param target
+     * @return
+     */
+    public int binarySearchVariant2(int[] sortedArray,int target){
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (low <= high){
+            int mid = low + ((high - low) >> 1);
+            if (sortedArray[mid] > target){
+                high = mid - 1;
+            }else if (sortedArray[mid] < target){
+                low = mid + 1;
+            }else {
+                if (mid == sortedArray.length-1 || sortedArray[mid+1] != sortedArray[mid]){
+                    return mid;
+                }else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 变体2：查找第一个大于等于给定值的元素
+     * @param sortedArray
+     * @param target
+     * @return
+     */
+    public int binarySearchVariant3(int[] sortedArray,int target){
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (low <= high){
+            int mid = low + ((high - low) >> 1);
+            if (sortedArray[mid] >= target){
+                if (mid == 0 || sortedArray[mid-1] < target){
+                    return mid;
+                }else {
+                    high = mid - 1;
+                }
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 变体2：查找最后一个小于等于给定值的元素
+     * @param sortedArray
+     * @param target
+     * @return
+     */
+    public int binarySearchVariant4(int[] sortedArray,int target){
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (low <= high){
+            int mid = low + ((high - low) >> 1);
+            if (sortedArray[mid] > target){
+                high = mid - 1;
+            }else {
+                if (mid == sortedArray.length-1 || sortedArray[mid+1] > target){
+                    return mid;
+                }else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
